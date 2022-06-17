134d4d4d6a4c19fdd1021efdd9f578f330e89ea3
jkingportfolio <mycodejourney3@gmail.com>
2022-06-17 16:44:52 +0000

    javascript now opens the correct modals, but needs work on the close as the close function only works for new game

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,42 @@
 
 document.getElementById("#newGameModal").addEventListener('click',
 function(){
-    document.querySelector('.bg-modal').style.display = "Flex";
-});
-
-document.getElementById("#helpModal").addEventListener('click',
-function(){
-    document.querySelector('.bg-modal').style.display = "Flex";
-});
-
-document.getElementById("#settingsModal").addEventListener('click',
-function(){
-    document.querySelector('.bg-modal').style.display = "Flex";
-});
-
-document.getElementById("#contactModal").addEventListener('click',
-function(){
-    document.querySelector('.bg-modal').style.display = "Flex";
+    document.querySelector('.new-game-modal').style.display = "Flex";
 });
 
 document.querySelector('.close').addEventListener('click', 
 function(){
-    document.querySelector('.bg-modal').style.display = "None";
+    document.querySelector('.new-game-modal').style.display = "None";
 });
+
+document.getElementById("#helpModal").addEventListener('click',
+function(){
+    document.querySelector('.help-modal').style.display = "Flex";
+});
+
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.help-modal').style.display = "None";
+});
+
+document.getElementById("#settingsModal").addEventListener('click',
+function(){
+    document.querySelector('.settings-modal').style.display = "Flex";
+});
+
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.settings-modal').style.display = "None";
+});
+
+document.getElementById("#contactModal").addEventListener('click',
+function(){
+    document.querySelector('.contact-modal').style.display = "Flex";
+});
+
+document.querySelector('.close').addEventListener('click', 
+function(){
+    document.querySelector('.contatc-modal').style.display = "None";
+});
+
+
