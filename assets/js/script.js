d0b88ca7b635baf8475e7daa493a754263480e6f
jkingportfolio <mycodejourney3@gmail.com>
2022-06-17 16:12:35 +0000

    javascript that was added last commit updated to test if all buttons on nav bar work for opening and closing the same single modale i have created at present

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,21 @@ function(){
     document.querySelector('.bg-modal').style.display = "Flex";
 });
 
+document.getElementById("#helpModal").addEventListener('click',
+function(){
+    document.querySelector('.bg-modal').style.display = "Flex";
+});
+
+document.getElementById("#settingsModal").addEventListener('click',
+function(){
+    document.querySelector('.bg-modal').style.display = "Flex";
+});
+
+document.getElementById("#contactModal").addEventListener('click',
+function(){
+    document.querySelector('.bg-modal').style.display = "Flex";
+});
+
 document.querySelector('.close').addEventListener('click', 
 function(){
     document.querySelector('.bg-modal').style.display = "None";
